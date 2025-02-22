c7141300e81e55cf9cdea766d14e4fb17b1267d7
jpb <jakub.baranski.33@gmail.com>
2025-02-22 12:09:09 +0000

    Begin implementing option toggling

--- UltraTextEdit/Views/MainPage.xaml.cs
+++ UltraTextEdit/Views/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Input;
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +12,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,8 +31,21 @@ namespace UltraTextEdit.Views
         public MainPage()
         {
             this.InitializeComponent();
+            this.KeyDown += OnKeyComboPressed;
+            editor.KeyDown += OnKeyComboPressed;
         }
 
+        private void OnKeyComboPressed(object sender, KeyRoutedEventArgs e)
+        {
+            var state = InputKeyboardSource.GetKeyStateForCurrentThread(Windows.System.VirtualKey.Control);
+            var isCtrlPressed = state.HasFlag(Windows.UI.Core.CoreVirtualKeyStates.Down);
+            if (e.Key == VirtualKey.B && isCtrlPressed)
+            {
+                BoldButton.IsChecked = !BoldButton.IsChecked;
+            }
+        }
+
+        #region Stub methods
         private void OpenEditTab(object sender, RoutedEventArgs e)
         {
 
@@ -40,7 +58,13 @@ namespace UltraTextEdit.Views
 
         private void BoldButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ITextSelection selectedText = editor.Document.Selection;
+            if (selectedText != null)
+            {
+                ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
+                charFormatting.Bold = FormatEffect.Toggle;
+                selectedText.CharacterFormat = charFormatting;
+            }
         }
 
         private void ItalicButton_Click(object sender, RoutedEventArgs e)
@@ -52,5 +76,6 @@ namespace UltraTextEdit.Views
         {
 
         }
+        #endregion
     }
 }
