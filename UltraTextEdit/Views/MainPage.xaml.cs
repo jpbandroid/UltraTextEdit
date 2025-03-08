a4088d20a2c1f8f14f4d30b78d86f68103690eec
jpb <errortek09@gmail.com>
2025-03-08 12:45:29 +0000

    Code cleanup

--- UltraTextEdit/Views/MainPage.xaml.cs
+++ UltraTextEdit/Views/MainPage.xaml.cs
@@ -1,58 +1,17 @@
-using Microsoft.UI.Input;
 using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Xml.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.System;
-using Windows.UI.Core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace UltraTextEdit.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void BoldButton_Click(object sender, RoutedEventArgs e)
-        {
-            ITextSelection selectedText = editor.Document.Selection;
-            if (selectedText != null)
-            {
-                ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
-                charFormatting.Bold = FormatEffect.Toggle;
-                selectedText.CharacterFormat = charFormatting;
-            }
-        }
-
-        private void ItalicButton_Click(object sender, RoutedEventArgs e)
-        {
-            ITextSelection selectedText = editor.Document.Selection;
-            if (selectedText != null)
-            {
-                ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
-                charFormatting.Italic = FormatEffect.Toggle;
-                selectedText.CharacterFormat = charFormatting;
-            }
         }
 
         #region Stub methods
@@ -101,7 +60,7 @@ namespace UltraTextEdit.Views
                     args.Handled = true;
                     break;
                 case Windows.System.VirtualKey.S:
-                    //SaveFile(false);
+                    args.Handled = true;
                     break;
             }
         }
